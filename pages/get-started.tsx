aa4d7a7c34cf13637463f8da82dc2b86cd2b88a1
Surya Teja Reddy <74102066+surya-teja-222@users.noreply.github.com>
2022-07-27 16:03:08 +0530

    Home Page

---
+++ pages/get-started.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+
+const Start: NextPage = () => {
+  return (
+    <div className=" h-[100vh] w-[100vw]">
+      <Head>
+        <title>Wysa - Home </title>
+        <meta
+          name="description"
+          content="Wysa Mini Project by Surya Teja Reddy "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Start;
